c0ffb25c696475ad651604106a29e91e22739aeb
niraj1234 <niraj.webtech@gmail.com>
2025-05-06 16:33:47 +0530

    06-05-2025_16:25 | Contact and About page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Products from './components/products/Products'
 import { BrowserRouter  as Router , Routes ,Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import NavBar from './components/shared/NavBar';
+import About from './components/About';
+import Contact from './components/Contact';
 
 
 const notify = () => toast('This is your Toast !');
@@ -15,8 +17,10 @@ function App() {
     <Router>
         <NavBar/>
         <Routes>
-        <Route  path='/' element={<Home/>}/>
-        <Route  path='/products' element={<Products/>}/>
+          <Route  path='/' element={<Home/>}/>
+          <Route  path='/products' element={<Products/>}/>
+          <Route  path='/about' element={<About/>}/>
+          <Route  path='/contact' element={<Contact/>}/>
         </Routes>
     </Router>
 
