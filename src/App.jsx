7155a34b6d64d8aa6a762a658ced932327812fe7
niraj1234 <niraj.webtech@gmail.com>
2025-05-06 11:51:01 +0530

    06-05-2025_11:25 | Home page carausel and Product display done HeroBanner adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,32 @@ import './App.css'
 import { FaBeer } from 'react-icons/fa'
 import toast , {Toaster} from 'react-hot-toast'
 import Products from './components/products/Products'
+import { BrowserRouter  as Router , Routes ,Route } from 'react-router-dom';
+import Home from './components/home/Home';
+
 
 const notify = () => toast('This is your Toast !');
 
 function App() {
   return (
-    <div className='bg-gray-200'>
-        <div className='text-amber-300 text-2xl font-bold bg-gray-700 p-2 border-b-4 border-b-cyan-700'>Meri Dukaan</div>
-        <div className=''>
-          <Products/>
-        </div>
-        
-    </div>
-  )
+
+    <Router>
+        <Routes>
+        <Route  path='/' element={<Home/>}/>
+        <Route  path='/products' element={<Products/>}/>
+        </Routes>
+    </Router>
+
+
+    // <div className='bg-gray-200'>
+    //     <div className='text-amber-300 text-2xl font-bold bg-gray-700 p-2 border-b-4 border-b-cyan-700'>Meri Dukaan</div>
+    //     <div className=''>
+    //       <Products/>
+    //     </div>        
+    // </div>
+
+
+)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
+          <App />
       ,
   </Provider>  
 )
